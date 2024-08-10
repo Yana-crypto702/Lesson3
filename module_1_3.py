9c3c3385d4df078c58b766cfc96188fbf39da17c
Yana-crypto702 <yanaorlova_1@icloud.com>
2024-08-10 15:38:32 +0400

    Update module_1_3.py

--- module_1_3.py
+++ module_1_3.py
@@ -2,7 +2,7 @@ name='Yana'
 print('Name:',name)
 age=19
 print('Age:',age)
-new_age=age+1
-print('New Age:', new_age)
+age=age+1
+print('New Age:', age)
 is_student= True
 print('Is Student:', is_student)
